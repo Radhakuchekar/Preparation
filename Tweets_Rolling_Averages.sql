580ce2f37ff553822e12ed6c671ca8ddc34c6dec
Radha Kuchekar <48984312+Radhakuchekar@users.noreply.github.com>
2025-01-24 08:40:41 +0530

    Create Tweets_Rolling_Averages.sql

---
+++ Tweets_Rolling_Averages.sql
@@ -0,0 +1,16 @@
+-- Tweets' Rolling Averages
+-- reference https://datalemur.com/questions/rolling-average-tweets
+
+SELECT 
+    user_id, 
+    tweet_date,
+    ROUND(AVG(tweet_count) OVER (
+        PARTITION BY user_id 
+        ORDER BY tweet_date 
+        ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+    ), 2) AS rolling_avg_3d
+FROM 
+    tweets;
+
+
+-- 
