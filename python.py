41f408f3c13b8c4496fccd5cc820a55999edeca3
Radha Kuchekar <48984312+Radhakuchekar@users.noreply.github.com>
2025-01-29 07:47:45 +0530

    type hints

--- python.py
+++ python.py
@@ -10,3 +10,23 @@ else:
 # String manipulation
 s ="abc"
 s[i] = s #not allowed 
+
+# Type Hints in python
+from typing import List, Tuple, Dict, Set
+
+numbers: List[int] = [1, 2, 3]
+data: Tuple[int, str] = (10, "Hello")
+score: Dict[str, int] = {"Alice": 90, "Bob": 85}
+unique_values: Set[str] = {"apple", "banana"}
+def foo(value: int | str) -> None: # either int or str 
+    print(value)
+
+from typing import TypeAlias
+Vector: TypeAlias = list[float]
+def scale_vector(v: Vector, factor: float) -> Vector:
+    return [x * factor for x in v]
+
+from typing import Callable
+def execute_operation(x: int, y: int, operation: Callable[[int, int], int]) -> int: #Callable[[int, int], int] means a function that takes two int arguments and returns an int
+    return operation(x, y)
+print(execute_operation(5, 3, lambda a, b: a + b))  # Output: 8
